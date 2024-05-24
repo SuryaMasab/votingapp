1b5f77ceebf8927224164944b9d79a94f0a7c88f
SuryaMasab <surya.masabathuala@gmail.com>
2024-05-24 20:32:13 +0530

    SM : Removed Unused methods
    SM : Removed Unused methods

--- VotersApi/Controllers/CandidateController.cs
+++ VotersApi/Controllers/CandidateController.cs
@@ -42,48 +42,4 @@ public class CandidateController(ICandidateRepository candidateRepository, ILogg
         return CreatedAtAction(nameof(GetCandidate), new { id = candidate.Id }, candidate);
     }
 
-    [HttpPut("{id}")]
-    public async Task<ActionResult<Candidate>> UpdateCandidate(int id, Candidate candidate)
-    {
-        try
-        {
-
-            if (id != candidate.Id)
-            {
-                return BadRequest();
-            }
-
-            var result = await _candidateRepository.GetCandidate(id);
-            if (result == null)
-            {
-                return NotFound("Candidate not found");
-            }
-            else
-            {
-                var updatedCandidate = await _candidateRepository.UpdateCandidate(candidate);
-                return Ok(updatedCandidate);
-            } 
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError($"From Candidate Controller {ex.Message} {ex.StackTrace}");
-        }
-        return BadRequest();
-    } 
-
-    [HttpDelete("{id}")]
-    public async Task<ActionResult<bool>> DeleteCandidate(int id)
-    {
-        var candidate = await _candidateRepository.GetCandidate(id);
-        if (candidate == null)
-        {
-            return NotFound();
-        }
-        var isSuccess= await _candidateRepository.DeleteCandidate(id);
-        if (isSuccess)
-        {
-            return Ok();
-        }
-        return NoContent();
-    }
 }
