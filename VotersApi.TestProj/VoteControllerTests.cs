907101d61860450dd31bf33e46aca897969d71cf
SuryaMasab <surya.masabathuala@gmail.com>
2024-05-24 20:29:34 +0530

    SM: Added UnitTests for Vote and VoterController
    SM: Added UnitTests for Vote and VoterController

---
+++ VotersApi.TestProj/VoteControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using VoterApp.Domain.Models;
+using VotersApi.Controllers;
+using VotersApi.Repository.Interfaces;
+
+namespace VotersApi.TestProj;
+
+[TestClass]
+public class VoteControllerTests
+{
+    [TestMethod]
+    public async Task CastVote_ReturnsOkResult()
+    {
+        // Arrange
+        var mockVoterRepo = new Mock<IVoterRepository>();
+        var mockCandidateRepo = new Mock<ICandidateRepository>();
+        var mockCastVoteRepo = new Mock<ICastVoteRepository>();
+        var logger = new Mock<ILogger<VoteController>>();
+        var controller = new VoteController(mockVoterRepo.Object, mockCandidateRepo.Object, mockCastVoteRepo.Object, logger.Object);
+        var newVote = new Vote { VoterId = 1, CandidateId = 1 };
+
+        mockVoterRepo.Setup(repo => repo.GetVoter(1))
+            .ReturnsAsync(new Voter { Id = 1, HasVoted = false });
+
+        _ = mockCandidateRepo.Setup(repo => repo.GetCandidate(1))
+            .ReturnsAsync(new Candidate { Id = 1, Name="CandidateName", VotesCount = 0 });
+
+        mockCastVoteRepo.Setup(repo => repo.CastVote(newVote))
+            .Returns(true);
+
+        // Act
+        var result = await controller.CastVote(newVote);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+        Assert.IsNotInstanceOfType(result.Result, typeof(BadRequestResult));
+    }
+}
--- VotersApi/Controllers/VoterController.cs
+++ VotersApi/Controllers/VoterController.cs
@@ -1,29 +1,38 @@
 ﻿using VoterApp.Domain.Models;
+using VotersApi.Repository;
+using VotersApi.Repository.Interfaces;
 
 namespace VotersApi.Controllers;
 
 [Route("api/[controller]")]
-public class VoterController(VoteAppDbContext context, ILogger<VoteController> logger) : ControllerBase
+public class VoterController(IVoterRepository VoterRepository, ILogger<VoterController> logger) : ControllerBase
 {
-    private readonly VoteAppDbContext _context = context;
-    private readonly ILogger<VoteController> _logger = logger;
-     
+    private readonly IVoterRepository _voterRepository = VoterRepository;
+
+    private readonly ILogger<VoterController> _logger = logger;
+
+
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Voter>>> GetVoters()
     {
-        return await _context.Voters.ToListAsync();
+        var result = await _voterRepository.GetVoters();
+        if (result == null)
+        {
+            return NotFound();
+        }
+        return Ok(result);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Voter>> GetVoter(int id)
-    {
-        var voter = await _context.Voters.FindAsync(id);
+    {       
+        var voter = await _voterRepository.GetVoter(id);
         if (voter == null)
         {
             _logger.LogInformation("Get Voter By Id failed {id}", id);
             return NotFound();
         }
-        return voter;
+        return Ok(voter);
     }
 
     [HttpPost]
@@ -31,47 +40,11 @@ public class VoterController(VoteAppDbContext context, ILogger<VoteController> l
     {
         if (voter == null)
         {
-            _logger.LogInformation("Invalid Add Voter Object");
-            return NotFound();
-        }
-        _context.Voters.Add(voter);
-        await _context.SaveChangesAsync();       
-        return CreatedAtAction(nameof(GetVoter), new { id = voter.Id }, voter);
-    }
-
-
-    [Obsolete]
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateVoter(int id, Voter voter)
-    {
-        if (id != voter.Id)
-        {
-            _logger.LogInformation("Update/Put Voter could not be found");
             return BadRequest();
         }
-        _context.Entry(voter).State = EntityState.Modified;
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (Exception ex)
-        {             
-            _logger.LogInformation($"Exception Occured Vide update {ex.Message} {ex.StackTrace}");
-        }
-        return NoContent();
-    }
+        var result = await _voterRepository.AddVoter(voter);
 
-    [Obsolete]
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteVoter(int id)
-    {
-        var voter = await _context.Voters.FindAsync(id);
-        if (voter == null)
-        {
-            return NotFound();
-        }
-        _context.Voters.Remove(voter);
-        await _context.SaveChangesAsync();
-        return NoContent();
+        return CreatedAtAction(nameof(GetVoter), new { id = voter.Id }, voter);        
     }
+     
 }
